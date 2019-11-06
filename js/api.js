5573e20a39b366607340d17797245d2e2f361b29
sahil <sahilchhabra019@gmail.com>
2019-11-06 12:25:10 +0530

    2nd phase of assignemnt
    popup  data and show more

--- js/api.js
+++ js/api.js
@@ -6,14 +6,30 @@ export const getLatestMovieDetails = async () => {
     return fetch(latest_url).then(response => response.json());
 }
 
+export const getMostWatchedMovieDetails = async () => {
+    const mostWatched_url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    return fetch(mostWatched_url).then(response => response.json());
+}
+
+export const getTrendingMovieDetails = async () => {
+    const trending_url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY} `;
+    return fetch(trending_url).then(response => response.json());
+}
+
 export const getMovieGenres = async () => {
     const genres_url =  `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`;
     return fetch(genres_url).then(response => response.json());
 }
 
-
-
 export const getMovieDetails = async (movieId) => {
     const movieDetailsUrl =  `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US&append_to_response=credits`;
     return fetch(movieDetailsUrl).then(response => response.json());
 }
+
+  export function ratingStar(rating) {
+      let ratingData = ''
+      for (let i = 0; i <= 4; i++) {
+        ratingData = `${ratingData} <i class= "fa ${rating >= i ?'fa-star' : 'fa-star-o'}"></i>`
+      }
+      return ratingData;
+    }
--- js/movie-details.js
+++ js/movie-details.js
@@ -1,4 +1,5 @@
 import {
+    ratingStar,
     getMovieDetails
 } from './api.js';
 
@@ -7,7 +8,7 @@ const image_base_url = 'https://image.tmdb.org/t/p/w500/';
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
-        window.location.search
+    window.location.search
         .substr(1)
         .split("&")
         .forEach(function (item) {
@@ -20,7 +21,7 @@ function findGetParameter(parameterName) {
 async function populateLatestData() {
     const id = findGetParameter('id');
 
-    if(id){
+    if (id) {
         const movieDetails = await getMovieDetails(id);
         console.log('movieDetails', movieDetails);
 
@@ -30,6 +31,31 @@ async function populateLatestData() {
         const node = document.importNode(details, true);
         const description = node.querySelector('.para-text');
         description.append(document.createTextNode(movieDetails.overview))
+
+        const movieTitle = node.querySelector('.primary-text')
+        movieTitle.append(document.createTextNode(movieDetails.original_title))
+
+        const moviePoster = node.querySelector('.full__banner figure img');
+        moviePoster.setAttribute("src", image_base_url + movieDetails.poster_path);
+        moviePoster.setAttribute("alt", movieDetails.original_title);
+        moviePoster.setAttribute("title", movieDetails.original_title);
+
+        let genre = '';
+        movieDetails.genres.map(genreItem => genre += genreItem.name + ', ');
+        const genredata = node.querySelector('.genre__data td');
+        genredata.append(document.createTextNode(genre));
+
+        let cast = '';
+        movieDetails.credits.cast.slice(0, 8).map(item => cast += item.name + ', ');
+        cast = cast.slice(0, -2);
+
+        const castData = node.querySelector('.cast__data td');
+        castData.append(document.createTextNode(cast))
+
+        //for movie rating
+        let ratingMovies = Math.round((movieDetails.vote_average / 2));
+        const rating = node.querySelector('.movie__ratingStars span')
+        rating.innerHTML = ratingStar(ratingMovies);
 
         document.getElementById('main-details').append(node);
     }
